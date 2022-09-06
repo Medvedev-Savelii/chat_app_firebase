c08eb6d490554294b2d6923b61038c015cdc7389
sava <savamedvedev@gmail.com>
2022-09-06 12:12:44 +0800

    third-commit

---
+++ src/components/Chats.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Chats(props) {
+    return (
+        <div><h1>Chats</h1></div>
+    );
+}
+
+export default Chats;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,10 +11,48 @@ function Register() {
     const [err, setErr] = useState(false);
     const navigate = useNavigate()
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
 
-    }
+        try {
+            //Create user
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+            const storageRef = ref(storage, displayName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                //TODO:Handle Percentage
+                (error) => {
+                    setErr(true);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                        //Update profile
+                        await updateProfile(res.user, {
+                            displayName,
+                            photoURL: downloadURL,
+                        });
+                        //create user on firestore
+                        await setDoc(doc(db, "users", res.user.uid), {
+                            uid: res.user.uid,
+                            displayName,
+                            email,
+                            photoURL: downloadURL,
+                        });
+                        //create empty user chats on firestore
+                        await setDoc(doc(db, "userChats", res.user.uid), {});
+                        navigate("/");
+                    });
+                }
+            );
+        } catch (err) {
+            setErr(true);
+        }
+    };
+
 
     return (
         <div className="formContainer">
