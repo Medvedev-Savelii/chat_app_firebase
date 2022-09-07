d5ef93b11951ad6587c6c1ccf3fc6d38002a32d3
sava <savamedvedev@gmail.com>
2022-09-07 17:57:43 +0800

    fives-commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import photoURL from "../img/userAvatar.jpg"
+
 
 const Message = () => {
+    const { currentUser } = useContext(AuthContext);
+    const { data } = useContext(ChatContext);
+
+    const ref = useRef();
+
     return (
-        <div>
-            <h1>Message</h1>
+        <>
+        <div
+            ref={ref}
+            className="message"
+        >
+            <div className="messageInfo">
+                <img
+                    src={photoURL}
+                    alt=""
+                />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                {/*{message.img && <img src={message.img} alt="" />}*/}
+            </div>
         </div>
+
+    <div
+        ref={ref}
+        className="message owner"
+    >
+        <div className="messageInfo">
+            <img
+                src={photoURL}
+                alt=""
+            />
+            <span>just now</span>
+        </div>
+        <div className="messageContent">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            {/*{message.img && <img src={message.img} alt="" />}*/}
+        </div>
+    </div>
+</>
     );
 };
 
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
+import Message from "./Message";
 
 const Messages = () => {
+    const [messages, setMessages] = useState([]);
+    const { data } = useContext(ChatContext);
+
     return (
-        <div>
-            <h1>Messages</h1>
+        <div className="messages">
+                <Message />
+                <Message />
+                <Message />
+                <Message />
+                <Message />
         </div>
     );
 };
